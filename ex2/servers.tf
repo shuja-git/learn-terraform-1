1b58869365c7e7199a3b8d2cf30419ffbbb10ac5
shuja-git <shuja.devops@gmail.com>
2023-08-11 22:40:46 -0700

    count.index

--- ex2/servers.tf
+++ ex2/servers.tf
@@ -9,6 +9,9 @@ resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0ee48eaa134172d7d"]
+  tags = {
+    Name = var.instances[count.index]
+  }
  }
  variable "instances" {
    default = [ "cart", "catalogue", "shipping", "payment", "user"]
