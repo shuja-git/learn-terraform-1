86c4940751cf61476fb645244bfd0fe309697514
shuja-git <shuja.devops@gmail.com>
2023-08-11 21:30:04 -0700

    state

---
+++ ex1/state.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "devopsb71-bucket"
+    key    = "remote-s3/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0ee48eaa134172d7d"]
+  tags = {
+    Name = "frontend"
+  }
+}
