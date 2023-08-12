2658a543f88b999d9101f50261eb2f8c85ec5b84
shuja-git <shuja.devops@gmail.com>
2023-08-11 23:10:33 -0700

    for_each

---
+++ roboshop-setup/servers/servers.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "instance" {
+  for_each = var.instances
+  ami           = data.aws_ami.ami.image_id
+  instance_type = each.value["type"]
+  vpc_security_group_ids = ["sg-0ee48eaa134172d7d"]
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+        name = "catalogue"
+        type = "t3.micro"
+    },
+    user = {
+        name = "user"
+        type = "t2.micro"
+    }
+  }
+}
