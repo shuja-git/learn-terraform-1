54523a5530e183132e857dd6b9f307fc120ad38d
shuja-git <shuja.devops@gmail.com>
2023-08-12 01:18:22 -0700

    changed ref

--- ex1/state.tf
+++ ex1/state.tf
@@ -11,7 +11,7 @@ data "aws_ami" "ami" {
   owners           = ["973714476881"]
 }
 
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0ee48eaa134172d7d"]
