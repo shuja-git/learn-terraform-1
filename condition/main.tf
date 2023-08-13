94dc42b902d77ef038752f19b301c983b5b9abbb
shuja-git <shuja.devops@gmail.com>
2023-08-12 22:35:55 -0700

    condition ex

--- condition/main.tf
+++ condition/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "test" {
+  count = var.create_instance == "true" ? 1 : 0
   ami = "ami-03265a0778a880afb"
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
 }
 
 variable "instance_type" {}
-
+variable "create_instance" {}
