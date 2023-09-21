3305f3a4d1bbfb19fac2f3a98950342f7983aea9
shuja-git <shuja.devops@gmail.com>
2023-09-21 02:11:45 -0700

    added sg and ec2 tag

--- prometheus-test-server/main.tf
+++ prometheus-test-server/main.tf
@@ -1,17 +1,29 @@
 resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0a962a9c6222cf89e"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0ee48eaa134172d7d"]
   wait_for_fulfillment = true
   tags = {
     Name = "prometheus-test-server"
   }
 }
+resource "aws_ec2_tag" "example" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prometheus-test-server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0a962a9c6222cf89e"
   instance_type = "t2.micro"
   wait_for_fulfillment = true
+  vpc_security_group_ids = ["sg-0ee48eaa134172d7d"]
   tags = {
     Name = "prometheus-test-node"
   }
 }
-
+resource "aws_ec2_tag" "example" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prometheus-test-node"
+}
