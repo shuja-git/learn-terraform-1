ea3c2b1b7271866d33eb6e5063c64ccc5921b6f6
shuja-git <shuja.devops@gmail.com>
2023-09-26 22:39:36 -0700

    testing spot instance

---
+++ self-testing/main.tf
@@ -0,0 +1,14 @@
+resource "aws_spot_instance_request" "node" {
+  ami           = "ami-0a962a9c6222cf89e"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0ee48eaa134172d7d"]
+  wait_for_fulfillment = true
+  tags = {
+    Name = "test-server"
+  }
+}
+resource "aws_ec2_tag" "node_tag" {
+  resource_id = aws_spot_instance_request.node.spot_instance_id
+  key         = var.monitor
+  value       = var.monitor ? "yes" : "no"
+}
---
+++ self-testing/vars.tf
@@ -0,0 +1,3 @@
+variable "monitor" {
+  default = "monitor"
+}
